d64044edade16336251d5aa6f2598ab2597dc2df
Deep Rakesh <deeptank09@gmail.com>
2026-01-31 10:23:19 +0530

    cleanup import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Portfolio from "./components/Portfolio";
 import Lenis from "lenis";
 import { useEffect } from "react";
+import "./App.css";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   useEffect(() => {
